65f36791cedac2272651b2d62cd6ae9ac025f51c
Unknown <auratus.lemma@gmail.com>
2017-04-06 03:41:08 +0900

    Add Mantle define file

---
+++ define.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+_CURRENTPATH = os.path.dirname(os.path.realpath(__file__))
+
+
+
+mantleIconPath = os.path.join(_CURRENTPATH, 'static', 'mantle_icon.svg')
+
+version = '0.1.0'
+
+windowName  = 'mantleWindow'
+windowTitle = "Mantle"
