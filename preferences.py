51d28f4b47a8254cbf34fb28c350081621cf502a
Unknown <auratus.lemma@gmail.com>
2017-04-09 03:23:58 +0900

    Apply setMovable toggle and setCloseable toggle.

--- preferences.py
+++ preferences.py
@@ -5,17 +5,44 @@ from PySide.QtUiTools import QUiLoader
 
 import define as DEFINE
 
+
+
 class Preferences(QtGui.QWidget):
+
 
     def __init__(self, parent=None):
         super(Preferences, self).__init__(parent)
         self._parent = parent
-        
+
         self.initGUI()
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.UI)
         self.setLayout(layout)
 
+        self.setSignals()
+
+
     def initGUI(self):
         loader = QUiLoader()
         self.UI = loader.load(DEFINE.preferencesUIPath)
+
+
+    def setSignals(self):
+        self.UI.setMovableCheckBox.clicked.connect(self.setMovableToggle)
+        self.UI.setCloseableCheckBox.clicked.connect(self.setClosableToggle)
+
+
+    def setMovableToggle(self):
+        toggle = self.UI.setMovableCheckBox.checkState()
+        if toggle:
+            self._parent.setMovable(True)
+        else:
+            self._parent.setMovable(False)
+
+
+    def setClosableToggle(self):
+        toggle = self.UI.setCloseableCheckBox.checkState()
+        if toggle:
+            self._parent.setTabsClosable(True)
+        else:
+            self._parent.setTabsClosable(False)
