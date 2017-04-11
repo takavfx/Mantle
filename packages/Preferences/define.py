b1963f00e5290c11654eefbd24fbce185bbcd8b4
Unknown <auratus.lemma@gmail.com>
2017-04-12 04:21:58 +0900

    Add config ini file name.

--- packages/Preferences/define.py
+++ packages/Preferences/define.py
@@ -2,6 +2,8 @@ import os
 
 _CURRENTPATH = os.path.dirname(os.path.realpath(__file__))
 
+config_name = 'mantle_config.ini'
+
 preferencesIconPath = os.path.join(_CURRENTPATH, 'static', 'gear.svg')
 preferencesUIPath = os.path.join(_CURRENTPATH, 'ui', 'preferences.ui')
 
